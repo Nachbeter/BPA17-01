145f86e8c1ce18f1e6b95e936fd38c4339ba1bf4
Nachbeter <55824401+Nachbeter@users.noreply.github.com>
2019-12-19 11:49:00 +0700

    Add files via upload

--- 7/4lab.java
+++ 7/4lab.java
@@ -1,92 +1,84 @@
+package lab4;
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
-import java.util.*;
-import java.math.*;
 
+public class Cat_class {
+     private static final Scanner scanner = new Scanner(System.in);
+      public static void main(String[] args) throws IOException {
+          
+          String[] xyz = scanner.nextLine().split(" ");
 
-public class Cat_class{
+                Animals coord = new Animals(Integer.parseInt(xyz[0]),Integer.parseInt(xyz[1]),Integer.parseInt(xyz[2]));
+                    
+                    
+            Solve runner=new Solve();
+            runner.running(coord);
+            
+            new Thread(runner::run).start();
 
-  private static final Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args) throws IOException {
-      
-      String[] xyz = scanner.nextLine().split(" ");
+            scanner.close();
 
-      		Animals coord = new Animals(Integer.parseInt(xyz[0]),Integer.parseInt(xyz[1]),Integer.parseInt(xyz[2]));
+        }
 
-            	//String result = Solve(catA, catB, catC).resh();
-        Solve runner=new Solve();
-        runner.running(coord);
+}
+class Solve
+{
+    private int x1;
+    private int y1;
+    private int z1;
+    
         
-        new Thread(runner::run).start();
+        public void run(){
+    
+            if (Math.abs(z1-x1)<Math.abs(z1-y1)) {System.out.println("Cat A");};
+            if (Math.abs(z1-x1)>Math.abs(z1-y1)) {System.out.println("Cat B");};
+            if (Math.abs(z1-x1)==Math.abs(z1-y1)) {System.out.println("Mouse C");};
+        }
 
-        scanner.close();
+        public void running(Animals x){
+    this.x1=x.getX();
+    this.y1=x.getY();
+    this.z1=x.getZ();
+    
+            }
+        }
 
+
+class Animals
+{
+    private int x;
+    private int y;
+    private int z;
+
+Animals(int xx, int vv, int zz){
+        x=xx;
+        y=vv;
+        z=zz;
     }
+
+
+    //setters
+public void setX(int newX){
+    this.x=newX;
+}
+public void setY(int newY){
+    this.y=newY;
 }
 
-class Solve
-    {
-	    private int x1;
-	    private int y1;
-	    private int z1;
-	    
-            
-            public void run(){
-        
-       			if (Math.Abs(z-x))<(Math.Abs(z-y)) {System.out.println("Cat A");};
-                if (Math.Abs(z-x))>(Math.Abs(z-y)) {System.out.println("Cat B");};
-                if (Math.Abs(z-x))=(Math.Abs(z-y)) {System.out.println("Mouse C");};
-    		}
+public void setZ(int newZ){
+    this.z=newZ;
+}
+//getters
+public int getX(){
+    return x;
+}
 
-    public void running(Animals x, Animals y, Animals z){
-        this.x1=x.getX();
-        this.y1=y.getY();
-        this.z1=z.getZ();
-        
-    }
-  
+public int getY(){
+    return y;
+}
+public int getZ(){
+    return z;
+}
 
-
-    }
-
-
-    class Animals
-    {
-    	private int x;
-    	private int y;
-    	private int z;
-
-	Animals(int xx, int vv, int zz){
-	        x=xx;
-	        v=vv;
-	        z=zz;
-	    }
-
-
-        //setters
-    public void setX(int newX){
-        this.x=newX;
-    }
-    public void setY(int newY){
-        this.y=newY;
-    }
-
-    public void setZ(int newZ){
-        this.z=newZ;
-    }
-    //getters
-    public int getX(){
-        return x;
-    }
-
-    public int getY(){
-        return y;
-    }
-    public int getZ(){
-        return z;
-    }
-
-    }
-
+}
 
